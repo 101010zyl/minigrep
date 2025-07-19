9d07314756af294bc59c7c19896fc8a5146e8540
Latiarch10 <zyl@Latiarch10>
2025-07-19 02:03:03 -0700

    Functionality done

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("With text: \n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 pub struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
 }
 
 impl Config {
@@ -19,5 +21,30 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query: query, file_path: file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
